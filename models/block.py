709067a251e578ebbdba3f0d91681ecea893bb41
mueru <mueru15@gmail.com>
2022-02-19 00:28:36 -0800

    Impl Decoder Fixes #6

---
+++ models/block.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+layers = keras.layers
+
+class LinerResBlock(keras.Model):
+	def __init__(self, units=256, activation=None, normalization=None, **kwargs):
+		def f():
+			return normalization() if normalization else layers.LayerNormalization()
+		self.activation = activation or layers.LeakyReLU()
+		super().__init__(name='LinerResBlock')
+		self.norm = [f() for _ in range(2)]
+		self.dense = [layers.Dense(units, activation, **kwargs) for _ in range(2)]
+
+	def call(self, input):
+		x = input
+		for n, d in zip(self.norm, self.dense):
+			x = n(x)
+			x = self.activation(x)
+			x = d(x)
+
+		return x + input
