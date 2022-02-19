1fe2afe088118d3b949fddf1d87076110f6e8e22
kodamanbou <kodamanbou0424@gmail.com>
2022-02-19 13:23:27 +0000

    add: static poses encoder(both hh and full-body)

--- models/encoder_hh_static.py
+++ models/encoder_hh_static.py
@@ -1,13 +1,23 @@
 import tensorflow as tf
-from tensorflow.keras import Model, models, layers
+from tensorflow.keras import Model, layers
 
-from models.block import LinerResBlock
+from models.block import LinearResBlock
 
 
 class StaticEncoder(Model):
-    def __init__(self, joints=3, res_num=3, latent_dim=256, activation=None, **kwargs):
+    def __init__(self, res_num=3, d=30, activation=None, **kwargs):
         super().__init__(name='Encoder_hh_Static')
         self.activation = activation or layers.LeakyReLU()
-        self.input = layers.Input(shape=(joints * 12,), name='')
         self.pre = layers.Dense(256)
-        self.resblocks = []
+        self.resblocks = [LinearResBlock(256, self.activation, **kwargs) for _ in range(res_num)]
+        self.post = layers.Dense(2 * d)
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.pre(inputs)
+        x = self.activation(x)
+        for b in self.resblocks:
+            x = b(x)
+        x = self.activation(x)
+        x = self.post(x)
+
+        return x
