bb5b934a78ff2aa7ed89c9da968d9ed779cc8941
mueru <mueru15@gmail.com>
2022-02-19 06:06:39 -0800

    sequence hh encoder #8

--- models/block.py
+++ models/block.py
@@ -4,12 +4,28 @@ layers = keras.layers
 
 class LinearResBlock(keras.Model):
 	def __init__(self, units=256, activation=None, normalization=None, **kwargs):
+		super().__init__(name='LinearResBlock')
+		self.b = ResBlock(lambda: layers.Dense(units, **kwargs), activation, normalization)
+
+	def call(self, input):
+		return self.b(input)
+
+class ConvResBlock(keras.Model):
+	def __init__(self, filters=256, kernel_size=3, activation=None, normalization=None, **kwargs):
+		super().__init__(name='ConvResBlock')
+		self.b = ResBlock(lambda: layers.Conv1D(filters, kernel_size, **kwargs), activation, normalization)
+
+	def call(self, input):
+		return self.b(input)
+
+class ResBlock(keras.Model):
+	def __init__(self, layer, activation=None, normalization=None):
 		def f():
 			return normalization() if normalization else layers.LayerNormalization()
 		self.activation = activation or layers.LeakyReLU()
-		super().__init__(name='LinerResBlock')
+		super().__init__(name='ResBlock')
 		self.norm = [f() for _ in range(2)]
-		self.dense = [layers.Dense(units, activation, **kwargs) for _ in range(2)]
+		self.dense = [layer() for _ in range(2)]
 
 	def call(self, input):
 		x = input
